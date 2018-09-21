1) Added GPU module. 2) Added correct coordinate transformation (up to scaling factor, it seems) (ExperimentalGeometry.py)

ExperimentalGeometry.py
@@ -57,7 +57,7 @@ class ScatteringGeometry:
         self._kf = misc.Delta( self._delta ) @ misc.Gamma( self._gamma ) @ self._ki         # scattered wave vector (3x1)
         self._Ghat = self._kf - self._ki
         self._Ghkl = ( self._Ghat / np.linalg.norm( self._Ghat ) ) * ( 
-            2.*np.pi*np.linalg.norm( self._peak ) / self._a0
+            2. * np.pi * np.linalg.norm( self._peak ) / self._a0
         ) # this is the 3x1 reciprocal lattice vector Ghkl around whith the diffraction pattern is centered
 
 
@@ -85,17 +85,11 @@ class ScatteringGeometry:
             # The columns of this matrix are the sampling steps in 3 independent directions in reciprocal space.
             # Two of these are perpendicular (i.e. detector plane) while the third is the rocking direction.
 
-        self._Bscope = self._Brecip * self._recip.repeat( 3, axis=0 )
-            # The columns of this matrix define a parallelopiped denoting the volume in reciprocal space
-            # queried during the entire scan.
+        Brange = np.diag( 1. / self._recip.ravel() )
 
-        self._Breal = ( 2. * np.pi / np.linalg.det( self._Bscope ) ) * np.cross( 
-            np.roll( self._Bscope, -1, axis=1 ), 
-            np.roll( self._Bscope, -2, axis=1 ), 
-            axisa=0, axisb=0
-        )
+        self._Breal = 2. * np.pi *\
+            np.linalg.inv( self._Brecip ).T @ Brange
             # The columns of this matrix are the sampling directions in 3D real space.
-            # Can also be computed as  2. * np.pi * np.linalg.inv( self._Brecip.T ).
 
         self._Brecip    *= 1.e-9    # scaling to nm^-1 units
         self._Breal     *= 1.e+9    # scaling to nm units

Phaser.py
@@ -41,6 +41,8 @@ from pyfftw.interfaces.numpy_fft import fftshift, fftn, ifftn
 from scipy.ndimage.filters import gaussian_filter
 from tqdm import tqdm
 
+import GPUModule as accelerator
+
 
 class Phaser:
 
@@ -48,8 +50,10 @@ class Phaser:
             modulus,
             support,
             beta=0.9, 
-            binning=1,
+            binning=1,      # for high-energy CDI. Set to 1 for regular phase retrieval.
             random_start=True, 
+            gpu=False,
+            outlog=''       # matters only for GPU
             ):
         self._modulus           = fftshift( modulus )
         self._support           = support
@@ -71,6 +75,12 @@ class Phaser:
         self._error             = []
         self._UpdateError()
 
+        if gpu==True:
+            self.gpusolver = accelerator.Solver( 
+                self.generateGPUPackage(),
+                outlog=outlog
+            )
+
 # Writer function to manually update support
     def UpdateSupport( self, support ):
         self._support = support
@@ -85,8 +95,12 @@ class Phaser:
         return
 
 # Reader function for the retrieved image
-    def Image( self ):
+    def finalImage( self ):
         return self._cImage
+
+# Reader function for estimated support
+    def finalSupport( self ):
+        return self._support
 
 # Reader function for the final computed modulus
     def Modulus( self ):
@@ -101,12 +115,16 @@ class Phaser:
 
 # Updating the error metric
     def _UpdateError( self ):
-        self._error += [ ( ( self._cImage_fft_mod - self._modulus )**2 * self._modulus ).sum() / self._modulus_sum ]
+        self._error += [ 
+            ( 
+                ( self._cImage_fft_mod - self._modulus )**2 * self._modulus 
+            ).sum() / self._modulus_sum 
+        ]
         return
 
 # Error reduction algorithm
     def ErrorReduction( self, num_iterations ):
-        for i in tqdm( list( range( num_iterations ) ), desc='ER' ):
+        for i in tqdm( list( range( num_iterations ) ), desc=' ER' ):
             self._ModProject()
             self._cImage *= self._support
             self._cImage_fft_mod = np.absolute( fftn( self._cImage ) )
@@ -118,13 +136,14 @@ class Phaser:
         for i in tqdm( list( range( num_iterations ) ), desc='HIO' ):
             origImage = self._cImage.copy() 
             self._ModProject()
-            self._cImage = ( self._support * self._cImage ) + self._support_comp * ( origImage - self._beta * self._cImage )
+            self._cImage = ( self._support * self._cImage ) +\
+                self._support_comp * ( origImage - self._beta * self._cImage )
             self._UpdateError()
         return
 
 # Solvent flipping algorithm
     def SolventFlipping( self, num_iterations ):
-        for i in tqdm( list( range( num_iterations ) ), desc='SF' ):
+        for i in tqdm( list( range( num_iterations ) ), desc=' SF' ):
             self._ModHatProject()
             self._ModProject()
             self._UpdateError()
@@ -132,7 +151,10 @@ class Phaser:
 
 # Basic shrinkwrap with gaussian blurring
     def ShrinkWrap( self, sigma, thresh ):
-        result = gaussian_filter( np.absolute( self._cImage ), sigma, mode='constant', cval=0. )
+        result = gaussian_filter( 
+            np.absolute( self._cImage ), 
+            sigma, mode='constant', cval=0.
+        )
         self._support = ( result > thresh*result.max() ).astype( float )
         self._support_comp = 1. - self._support
         return
@@ -140,7 +162,9 @@ class Phaser:
 # The projection operator into the modulus space of the FFT.
 # This is a highly nonlinear operator.
     def _ModProject( self ):
-        self._cImage = ifftn( self._modulus * np.exp( 1j * np.angle( fftn( self._cImage ) ) ) )
+        self._cImage = ifftn( 
+            self._modulus * np.exp( 1j * np.angle( fftn( self._cImage ) ) ) 
+        )
         return
 
 # The reflection operator in the plane of the (linear)
@@ -158,4 +182,14 @@ class Phaser:
         self._ModProject()
         self._SupReflect()
         return
+
+# Generates a package for the GPU module to read and generate tensors.
+    def generateGPUPackage( self ):
+        mydict = { 
+            'modulus':self._modulus, 
+            'support':self._support, 
+            'beta':self._beta, 
+            'cImage':self._cImage
+        }
+        return mydict
 