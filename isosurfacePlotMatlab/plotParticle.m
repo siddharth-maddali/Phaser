1a9daa3c97fc1490a709eab7481e2d1eed5cef3b
Siddarth Maddali <smaddali@alumni.cmu.edu>
2020-01-16 12:21:19 -0600

    Small improvements to plotParticle.m

--- isosurfacePlotMatlab/plotParticle.m
+++ isosurfacePlotMatlab/plotParticle.m
@@ -4,11 +4,13 @@ function [ iso, x, y, z, p, cbar ] = plotParticle( rho, varargin )
 %     1 = isoval (float) (default = 0.5)
 %     2 = skew (3x3 matrix) (real space sampling basis)
 %     3 = objSelect (string) ('straight': plots rho(x), 'twin': plots rho*(-x) )
+%     4 = smoothflag (string) ('smooth' or 'nosmooth' for phase)
 
     % first resolving all inputs
     isoval = 0.5;               %
     skew = eye( 3 );            %   defaults
     objSelect = 'straight';     %
+    smth = 'smooth';            %
 
     if nargin > 1
         isoval = varargin{1};
@@ -20,6 +22,9 @@ function [ iso, x, y, z, p, cbar ] = plotParticle( rho, varargin )
         objSelect = varargin{3};
     end
     if nargin > 4
+        smth = varargin{4};
+    end
+    if nargin > 5
         fprintf( 2, 'plotParticle warning: Too many input arguments. \n' );
     end
     
@@ -39,9 +44,9 @@ function [ iso, x, y, z, p, cbar ] = plotParticle( rho, varargin )
     end
     amp = abs( rhoFinal );
     ang = angle( rhoFinal );
-    ang = ang .* ( ang > 0 ) + ( 2*pi + ang ) .* ( ang < 0 ); % unwrapping
-    angmean = sum( ang(:) .* amp(:)  ) / sum( amp(:) );
-    ang = ang - angmean;
+%     ang = ang .* ( ang > 0 ) + ( 2*pi + ang ) .* ( ang < 0 ); % unwrapping
+%     angmean = sum( ang(:) .* amp(:)  ) / sum( amp(:) );
+%     ang = ang - angmean;
     rhoPlot = amp .* exp( 1j * ang );
     
     [ x, y, z ] = meshgrid( 1:size( rhoPlot, 1 ), 1:size( rhoPlot, 2 ), 1:size( rhoPlot, 3 ) );
@@ -55,12 +60,17 @@ function [ iso, x, y, z, p, cbar ] = plotParticle( rho, varargin )
     x = reshape( pts(1,:)', size( rhoPlot ) );
     y = reshape( pts(2,:)', size( rhoPlot ) );
     z = reshape( pts(3,:)', size( rhoPlot ) );
+    if strcmp( smth, 'smooth' )
+        colorField = smooth3( angle( rhoPlot ), 'gaussian', 1 );
+    else
+        colorField = angle( rhoPlot );
+    end
     
     iso = isosurface( ...
         x, y, z, ...
-        smooth3( smooth3( abs( rhoPlot ), 'gaussian', 5 ), 'gaussian', 5 ), ...
+        smooth3( smooth3( abs( rhoPlot ), 'gaussian', 3 ), 'gaussian', 3 ), ...
         isoval, ...
-        smooth3( angle( rhoPlot ), 'gaussian', 3 ) ...
+        colorField ...
     );
     iso.facevertexcdata = iso.facevertexcdata - mean( iso.facevertexcdata(:) );
     p = patch( iso, 'FaceColor', 'interp', 'EdgeColor', 'none' );
