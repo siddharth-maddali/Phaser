d6b8f3f9c1ce2f0750e6d0cd1cff165a352c4daa
Siddharth Maddali <siddharth.mv@protonmail.com>
2020-08-10 21:21:24 -0500

    Debugged safft module.

--- GAFFT.py
+++ GAFFT.py
@@ -27,15 +27,21 @@ except:
 
 class Mixin: 
 
+    def __init__( self, sup, Bq ): # overridden by inheriting class
+        self._support = fftshift( sup ) # the shifting is already done in the inheriting class.
+        self._Bq = Bq
+        self.setupDiffractionGeometry()
+        return
+
     def setupDiffractionGeometry( self ):
 
         self._grid = [ fftshift( this ) for this in np.meshgrid( *[ np.arange( -n//2., n//2. ) for n in self._support.shape ] ) ]
-        self._grid[1] = np.flip( self.grid[1], axis=0 )
+        #self._grid[1] = np.flip( self._grid[1], axis=0 )
 
         dq_tilde = np.sqrt( ( self._Bq**2 ).sum( axis=0 ) )
         self._Bq = self._Bq / dq_tilde.reshape( 1, -1 ).repeat( 3, axis=0 )
         N1, N2, N3 = self._grid[0].shape
-        al, bt, gm = tuple( Bq[:,-1] ) # detector-frame direction cosines of sampling vector along rocking direction
+        al, bt, gm = tuple( self._Bq[:,-1] ) # detector-frame direction cosines of sampling vector along rocking direction
         dq3 = gm * dq_tilde[2]
         
         dr3 = 1./ ( gm * N3 * dq_tilde[2] )
@@ -44,19 +50,25 @@ class Mixin:
 
         self.realSpaceSteps = dr1, dr2, dr3
 
-        self.__mu__     = np.exp( -1.j * 2. * np.pi * ( dq3*zs * ( (al/gm)*dr1*xs + (bt/gm)*dr2*ys ) ) )
-        self.__mustar__ = np.conj( self.__mu__ ) # one used for forward propagation, the other for backward propagation.
+        self.__mu__     = np.exp( -1.j * 2. * np.pi * ( 
+            dq3*self._grid[2] * ( 
+                (al/gm)*dr1*self._grid[0] + 
+                (bt/gm)*dr2*self._grid[1]
+            ) 
+        ) )
+        self.__mustar__ = np.conj( self.__mu__ )    # one used for forward propagation, the other for backward propagation.
+        self.__measure__ = dr1 * dr2 * dr3 * gm     # volume scaling measure when transforming between ortho and sheared spaces.
 
-        self._myfftn = gafftn         # new geometry-aware propagators
-        self._myifftn = gaifftn
+        self._myfftn = self.gafftn         # new geometry-aware propagators
+        self._myifftn = self.gaifftn
 
         return
     
 
     def gafftn( self, arr ):
-        arr_out = fftn( self.__mu__*fftn( arr, axes=[2] ), axes=[ 0, 1 ] )
+        arr_out = fftn( self.__mu__*fftn( arr, axes=[ 2 ] ), axes=[ 0, 1 ] ) * self.__measure__
         return arr_out
 
     def gaifftn( self, arr ):
-        arr_out = fftn( self.
+        arr_out = ifftn( self.__mustar__ * ifftn( arr, axes=[ 0, 1 ] ), axes=[ 2 ] ) / self.__measure__
         return arr_out
--- Phaser.py
+++ Phaser.py
@@ -48,14 +48,15 @@ from logzero import logger
 # plugin modules
 import Core, RecipeParser
 import ER, HIO, SF
-#import GAFFT
+import GAFFT
 
 class Phaser( 
         Core.Mixin,             # core CPU algorithms and routines
         RecipeParser.Mixin,     # for handling recipe strings
         ER.Mixin,               # error reduction
         HIO.Mixin,              # hybrid input/output
-        SF.Mixin                # solvent flipping
+        SF.Mixin,               # solvent flipping
+        GAFFT.Mixin
     ):
 
     def __init__( self,
@@ -79,14 +80,14 @@ class Phaser(
         self._support           = fftshift( support )
         self._beta              = beta
         
-#        if version=='diffgeom' and Brecip==None:
-#            logzero.error( 'Need to provide matrix Brecip in the detector frame.' )
-#            return
-#        else:
-#            self._Bq = Brecip
-#            self.setupDiffractionGeometry() # overwrites handles for some Core routines.
+        if version=='diffgeom' and Brecip==None:
+            logger.error( 'Need to provide matrix Brecip in the detector frame.' )
+            return
+        else:
+            self._Bq = Brecip
+            self.setupDiffractionGeometry() # overwrites handles for some Core routines.
 
-#        self._modulus_sum       = modulus.sum()
+        self._modulus_sum       = modulus.sum()
         self._support_comp      = 1. - support
         if random_start:
             self._cImage            = np.exp( 2.j * np.pi * np.random.rand( 
